ed36510c2e607c52d203b83fcc5e5cd531d6634c
LiquidPlummer <kyle.plummer@revature.com>
2023-03-14 12:26:09 -0400

    hash table solution

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,18 @@
+import java.util.Hashtable;
+import java.util.Map;
+
 public class Solution {
     public boolean containsDuplicate(int[] nums) {
         //solve problem here
+        //K = element, V = index
+        Map<Integer, Integer> integerMap = new Hashtable<>();
+        integerMap.put(nums[0], 0);
+
+        for(int i = 1; i < nums.length; i++) {
+            if(integerMap.put(nums[i], i) != null) {
+                return true;
+            }
+        }
 
         return false;
     }
