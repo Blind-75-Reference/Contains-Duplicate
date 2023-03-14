ab205bd64a07572bbc393f3a08c07ca7b5950c97
LiquidPlummer <kyle.plummer@revature.com>
2023-03-14 12:22:07 -0400

    sort first solution

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,14 @@
+import java.util.Arrays;
+
 public class Solution {
     public boolean containsDuplicate(int[] nums) {
-        //solve problem here
+        Arrays.sort(nums);
+
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] == nums[i - 1]) {
+                return true;
+            }
+        }
 
         return false;
     }
