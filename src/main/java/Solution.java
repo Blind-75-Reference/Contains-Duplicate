bb76415261271818f21a1898d2ef840c3e74564e
LiquidPlummer <kyle.plummer@revature.com>
2023-03-14 13:23:26 -0400

    notes

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,15 @@
 import java.util.Arrays;
 
 public class Solution {
+    /*
+    This solution first sorts the array, making it easy to determine if there are duplicates. Instead of looping through
+    the array multiple times in nested loops, we only need to pass through it once considering the next element. If
+    any two adjacent elements are the same value, we have found duplicates and return true. If we finish the loop
+    with no duplicates, we return false.
+
+    This adds the complexity of sorting the array, but makes the rest of the solution far more efficient than the
+    brute force solution. 
+     */
     public boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums);
 
